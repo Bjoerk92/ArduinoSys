e44ababd17403e491da92727eadc39c20d954f12
Anders <anders.bjoerk.unf@gmail.com>
2026-03-31 21:29:36 +0200

    [FIX] Driver interface issue

---
+++ Components/HWDrivers/DriverInterface/scr/AVR_Driver.c
@@ -0,0 +1,17 @@
+/**
+ * @file AVR_Driver.c
+ * @author Anders Bjørk (anders.bjoerk.unf@gmail.com)
+ * @brief Driver interface for the AVR microcontroller, providing functions for initializing and controlling the AVR microcontroller.
+ * @version 0.1
+ * @date 2026-03-31
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include "AVR_Driver.h"
+
+void Driver_Init(void)
+{
+    // Initialization code for the AVR driver
+}
