0bfdbae49ec2be15c538dae4a23043e575a2814a
Anders <anders.bjoerk.unf@gmail.com>
2026-04-03 19:09:37 +0200

    [ADD] added new Driver interface

---
+++ AVR_DRIVERS/AVR_Drivers.h
@@ -0,0 +1,17 @@
+/**
+ * @file AVR_Drivers.h
+ * @author Anders Bjørk (anders.bjoerk.unf@gmail.com)
+ * @brief Interface for AVR drivers.
+ * @version 0.1
+ * @date 2026-04-03
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#pragma once
+
+typedef enum DRV_STATUS_Etag
+{
+    DRV_STATUS_OK = 0,
+    DRV_STATUS_ERROR = 1,
+} DRV_STATUS_E;
