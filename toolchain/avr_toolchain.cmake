0e173437e2d99f71ebea0a2bec6039ea533da27f
Anders <anders.bjoerk.unf@gmail.com>
2026-07-21 20:46:07 +0200

    [fix] updated the avr toolchain

--- toolchain/avr_toolchain.cmake
+++ toolchain/avr_toolchain.cmake
@@ -1,38 +1,28 @@
 
-set(AVRCPP   avr-g++)
-set(AVRC     avr-gcc)
+set(CMAKE_SYSTEM_NAME  Generic)
+set(CMAKE_SYSTEM_PROCESSOR  avr)
+
+# Needs to come before the project function
+set(CMAKE_CXX_COMPILER avr-g++)
+set(CMAKE_C_COMPILER   avr-gcc)
+set(CMAKE_ASM_COMPILER avr-gcc)
+
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_C_STANDARD 11)
+
 set(AVRSTRIP avr-strip)
 set(OBJCOPY  avr-objcopy)
 set(OBJDUMP  avr-objdump)
 set(AVRSIZE  avr-size)
 set(AVRDUDE  avrdude)
 
-# Needs to come before the project function
-set(CMAKE_SYSTEM_NAME  Generic)
-set(CMAKE_CXX_COMPILER ${AVRCPP})
-set(CMAKE_C_COMPILER   ${AVRC})
-set(CMAKE_ASM_COMPILER ${AVRC})
-
-if(NOT DEFINED MCU)
-    if(TARGET_CHIP STREQUAL "ATmega328P")
-        set(MCU "atmega328p")
-    endif()
+if (TARGET_CHIP STREQUAL "ATmega328P")
+    set(MCU "atmega328p")
+    set(F_CPU "16000000UL")
+    set(BAUD "9600")
 endif()
 
-if(NOT DEFINED F_CPU)
-    if(TARGET_CHIP STREQUAL "ATmega328P")
-        set(F_CPU "16000000UL")
-    endif()
-endif()
-
-if(NOT DEFINED BAUD)
-    if(TARGET_CHIP STREQUAL "ATmega328P")
-        set(BAUD "9600")
-    endif()
-endif()
-
-set(CMAKE_CXX_STANDARD 11)
-set(CMAKE_C_STANDARD 11)
+add_definitions(-DF_CPU=${F_CPU})
 
 set(CSTANDARD "-std=gnu11")
 set(CPPSTANDARD "-std=gnu++11")
@@ -51,8 +41,3 @@ set(CMAKE_CXX_FLAGS "${CXXFLAGS}")
 set(CMAKE_ASM_FLAGS   "${CFLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAP}")
 
-if (TARGET_CHIP STREQUAL "ATmega328P")
-    set(MCU "atmega328p")
-    set(F_CPU "16000000UL")
-    set(BAUD "9600")
-endif()
